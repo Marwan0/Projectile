c8b14c6ec296aa1071cde2bcb4ff247cb5ae779a
Arash Bahrehmand <stdmwmrule@gmail.com>
2018-05-11 21:27:16 +0200

    Adding Animation...

--- Assets/Scripts/ProjectileUtility/ProjectileWrapper.cs
+++ Assets/Scripts/ProjectileUtility/ProjectileWrapper.cs
@@ -9,28 +9,54 @@ public class ProjectileWrapper : MonoBehaviour
 {
 	public float InitialAngle;
 	public float Duration;
-	public Transform TargetPos;
+	public Transform TargetTransform;
 
 	private Projectile projectile;
 	private Vector2 initialPos2D;
 	List<Vector2> pointsVec2=new List<Vector2>();
+	private bool isMoving;
 	
 	void Start ()
 	{
+		this.initialPos2D = this.transform.position;
+		
 		Projectile.InitialAngleDuration initialAngleDuration = new Projectile.InitialAngleDuration();
 		initialAngleDuration.InitAngle = this.InitialAngle;
 		initialAngleDuration.Duration = this.Duration;
 		
 		projectile = new Projectile(0.6f,initialAngleDuration);
-
+		TargetMovement.OnPositionChanged += StopAnimation;
 	}
 
 	void FixedUpdate () {
 		
-		List<Projectile.ProjectilePoint> projectilePoints = projectile.GetProjectileSamples(this.transform.position,this.TargetPos.position);
-		pointsVec2 = projectilePoints.Select(s => s.Position2D).ToList();
+		if (!this.isMoving)
+		{
+			List<Projectile.ProjectilePoint> projectilePoints = projectile.GetProjectileSamples(this.initialPos2D,this.TargetTransform.position);
+			pointsVec2 = projectilePoints.Select(s => s.Position2D).ToList();
+			StartCoroutine(Animate());
+		}
 	}
 
+	private void StopAnimation()
+	{
+		this.isMoving=false;
+		StopAllCoroutines();
+	}
+
+	IEnumerator Animate()
+	{
+		this.isMoving = true;
+
+		for (int i = 0; i < this.pointsVec2.Count; i++)
+		{
+			this.transform.position = this.pointsVec2[i];
+			yield return null;
+		}
+		this.isMoving = false;
+		this.transform.position = this.pointsVec2[0];
+	}
+	
 	void OnDrawGizmos()
 	{
 		for (int i = 0; i < this.pointsVec2.Count-1; i++)
@@ -38,5 +64,8 @@ public class ProjectileWrapper : MonoBehaviour
 			if (i%2==0)
 			Gizmos.DrawLine(this.pointsVec2[i],this.pointsVec2[i+1]);
 		}
+		
+		Gizmos.color=Color.magenta;
+		Gizmos.DrawLine(this.initialPos2D,this.TargetTransform.position);
 	}
 }
